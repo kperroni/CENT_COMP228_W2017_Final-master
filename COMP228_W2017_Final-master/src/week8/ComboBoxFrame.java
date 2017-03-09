3d2a20c2530102ddb5e053f3bcc696bd47f24eed
kperroni <kenny.perroni@gmail.com>
2017-03-09 09:51:01 -0500

    GUI
    ComboBoxFrame example

---
+++ COMP228_W2017_Final-master/src/week8/ComboBoxFrame.java
@@ -0,0 +1,61 @@
+package week8;
+
+import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ComboBoxFrame extends JFrame{
+	
+	private final JComboBox<String> imagesJComboBox;
+	private final JLabel label;
+	
+	private static final String[] names = 
+			{"bug1.png", "bug2.gif", "travelbug.gif", "buganim.gif"};
+	
+	private final Icon[] icons = 
+		{new ImageIcon("images/" + names[0]),
+		 new ImageIcon("images/" + names[1]),
+		 new ImageIcon("images/" + names[2]),
+		 new ImageIcon("images/" + names[3])};
+	
+	public ComboBoxFrame(){
+		
+		super("Testing JComboBox");
+		setLayout(new FlowLayout());
+		
+		imagesJComboBox = new JComboBox<String>(names);
+		imagesJComboBox.setMaximumRowCount(3);
+		
+		imagesJComboBox.addItemListener(
+				
+				new ItemListener()
+				{
+
+					@Override
+					public void itemStateChanged(ItemEvent e) {
+						
+						if(e.getStateChange() == ItemEvent.SELECTED){
+							
+							label.setIcon(icons[imagesJComboBox.getSelectedIndex()]);
+						}
+						
+					}
+					
+				} //End to our annoymous class
+				
+				); //End call to addItemListener
+		
+		add(imagesJComboBox);
+		label = new JLabel(icons[0]); //Default label icon
+		add(label);
+	}
+
+	
+	
+}
